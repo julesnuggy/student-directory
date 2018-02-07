2b211a48f5f40e03a8e8645a0685548320266c8f
julesnuggy <julesnuggy@gmail.com>
2018-02-07 11:40:04 +0000

    Updated directory.rb - added methods

--- directory.rb
+++ directory.rb
@@ -11,8 +11,20 @@ students = ["Dr. Hannibal Lecter",
   "Norman Bates"]
 
 # puts a list of all the students
-puts "The students of Villain Academy"
-puts "-------------------------------"
-students.each { |name| puts name }
+def print_header
+  puts "The students of Villain Academy"
+  puts "-------------------------------"
+end
+
+def print(stu_names)
+  stu_names.each { |name| puts name }
+end
+
 # print/puts how many students we have
-puts "Overall we have #{students.count} great students."
+def print_footer(names)
+  puts "Overall we have #{names.count} great students."
+end
+
+print_header
+print(students)
+print_footer(students)
