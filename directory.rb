000a0880ebf4a102bacaab35f4d271a48ebd1fab
julesnuggy <julesnuggy@gmail.com>
2018-02-07 11:29:55 +0000

    Updated directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# puts a list of all the students
 puts "The students of Villain Academy"
 puts "-------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+# print/puts how many students we have
+print "Overall we have 11"
+puts " great students."
