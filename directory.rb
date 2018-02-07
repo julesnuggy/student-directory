bd068cfd4d0d362b67c5493137014b1ee91785ca
julesnuggy <julesnuggy@gmail.com>
2018-02-07 11:50:36 +0000

    Updated directory.rb using hashes

--- directory.rb
+++ directory.rb
@@ -1,14 +1,15 @@
-students = ["Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex Delarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"]
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: "november"},
+  {name: "Darth Vader", cohort: "november"},
+  {name: "Nurse Ratched", cohort: "november"},
+  {name: "Michael Corleone", cohort: "november"},
+  {name: "Alex Delarge", cohort: "november"},
+  {name: "The Wicked Witch of the West", cohort: "november"},
+  {name: "Terminator", cohort: "november"},
+  {name: "Freddy Krueger", cohort: "november"},
+  {name: "The Joker", cohort: "november"},
+  {name: "Joffrey Baratheon", cohort: "november"},
+  {name: "Norman Bates", cohort: "november"}]
 
 # puts a list of all the students
 def print_header
@@ -17,7 +18,7 @@ def print_header
 end
 
 def print(stu_names)
-  stu_names.each { |name| puts name }
+  stu_names.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 # print/puts how many students we have
